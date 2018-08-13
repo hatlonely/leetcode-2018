a7167e43a030f6eb8d6367348c19ee8c01c8a1ba
hatlonely <hatlonely@gmail.com>
2018-08-13 20:46:47 +0800

    update c++/15_3sum.cpp

--- c++/15_3sum.cpp
+++ c++/15_3sum.cpp
@@ -5,16 +5,21 @@
 class Solution {
    public:
     std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-        if (nums.empty()) {
+        if (nums.size() < 3) {
             return std::vector<std::vector<int>>();
         }
 
         std::vector<std::vector<int>> result;
         std::sort(nums.begin(), nums.end());
-        for (size_t i = 1; i < nums.size() - 1;) {
-            size_t idx1 = 0;
-            size_t idx2 = nums.size() - 1;
-            while (idx1 < i && idx2 > i) {
+
+        size_t n = nums.size();
+        for (size_t i = 0; i < n - 2; i++) {
+            while (i > 0 && nums[i] == nums[i - 1]) {
+                i++;
+            }
+            size_t idx1 = i + 1;
+            size_t idx2 = n - 1;
+            while (idx2 > idx1) {
                 auto sum = nums[i] + nums[idx1] + nums[idx2];
                 if (sum < 0) {
                     idx1++;
@@ -27,7 +32,7 @@ class Solution {
                         idx2--;
                     }
                 } else {
-                    result.emplace_back(std::vector<int>({nums[idx1], nums[i], nums[idx2]}));
+                    result.emplace_back(std::vector<int>({nums[i], nums[idx1], nums[idx2]}));
                     idx1++;
                     while (idx1 < i && nums[idx1] == nums[idx1 - 1]) {
                         idx1++;
@@ -37,26 +42,6 @@ class Solution {
                         idx2--;
                     }
                 }
-            }
-            if (i < nums.size() - 2 && nums[i] == nums[i + 1] && nums[i] != nums[i - 1]) {
-                idx1 = i;
-                i++;
-                idx2 = nums.size() - 1;
-                while (idx2 > i) {
-                    auto sum = nums[i] + nums[idx1] + nums[idx2];
-                    if (sum > 0) {
-                        idx2--;
-                        continue;
-                    }
-                    if (sum == 0) {
-                        result.emplace_back(std::vector<int>({nums[idx1], nums[i], nums[idx2]}));
-                    }
-                    break;
-                }
-            }
-            i++;
-            while (i < nums.size() - 1 && nums[i] == nums[i - 1]) {
-                i++;
             }
         }
         return result;
@@ -114,5 +99,12 @@ TEST(testThreeSum, case7) {
     Solution solution;
     auto     nums   = std::vector<int>{0, 0, 0, 0};
     auto     result = std::vector<std::vector<int>>{{0, 0, 0}};
+    EXPECT_EQ(solution.threeSum(nums), result);
+}
+
+TEST(testThreeSum, case8) {
+    Solution solution;
+    auto     nums   = std::vector<int>{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6};
+    auto     result = std::vector<std::vector<int>>{{-4, -2, 6}, {-4, 0, 4}, {-4, 1, 3}, {-4, 2, 2}, {-2, -2, 4}, {-2, 0, 2}};
     EXPECT_EQ(solution.threeSum(nums), result);
 }
