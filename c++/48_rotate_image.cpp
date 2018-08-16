4846162bffdbf185385f05ef94f15a69493c576d
hatlonely <hatlonely@gmail.com>
2018-08-17 00:00:19 +0800

    add c++/48_rotate_image.cpp

---
+++ c++/48_rotate_image.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+class Solution {
+   public:
+    void rotate(std::vector<std::vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int m = matrix[i][j];
+                std::swap(m, matrix[j][n - 1 - i]);
+                std::swap(m, matrix[n - 1 - i][n - 1 - j]);
+                std::swap(m, matrix[n - 1 - j][i]);
+                std::swap(m, matrix[i][j]);
+            }
+        }
+    }
+};
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(test, case1) {
+    Solution solution;
+
+    auto matrix = std::vector<std::vector<int>>({{1, 2, 3},
+                                                 {4, 5, 6},
+                                                 {7, 8, 9}});
+
+    auto result = std::vector<std::vector<int>>({{7, 4, 1},
+                                                 {8, 5, 2},
+                                                 {9, 6, 3}});
+
+    solution.rotate(matrix);
+    EXPECT_EQ(matrix, result);
+}
