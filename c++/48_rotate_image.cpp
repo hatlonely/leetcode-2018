83919b6f5a861431af3410048f02b8f443e908b6
hatlonely <hatlonely@gmail.com>
2018-08-17 00:13:02 +0800

    add c++/48_rotate_image.cpp

--- c++/48_rotate_image.cpp
+++ c++/48_rotate_image.cpp
@@ -7,11 +7,11 @@ class Solution {
         int n = matrix.size();
         for (int i = 0; i < (n + 1) / 2; i++) {
             for (int j = 0; j < n / 2; j++) {
-                int m = matrix[i][j];
-                std::swap(m, matrix[j][n - 1 - i]);
-                std::swap(m, matrix[n - 1 - i][n - 1 - j]);
-                std::swap(m, matrix[n - 1 - j][i]);
-                std::swap(m, matrix[i][j]);
+                int m                        = matrix[i][j];
+                matrix[i][j]                 = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i]         = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i]         = m;
             }
         }
     }
