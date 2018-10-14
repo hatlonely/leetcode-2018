dc31d12d7fdca25a493e5c0a0c219e8009246e56
hatlonely <hatlonely@gmail.com>
2018-10-15 01:08:34 +0800

    add c++/95_unique_binary_search_trees_ii.cpp

---
+++ c++/95_unique_binary_search_trees_ii.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
+#include "treenode.hpp"
+
+class Solution {
+    std::vector<TreeNode*> generateTreesRecursive(int m, int n) {
+        if (m > n) {
+            return {nullptr};
+        }
+        if (m == n) {
+            return {new TreeNode(m)};
+        }
+        std::vector<TreeNode*> trees;
+        for (int i = m; i <= n; i++) {
+            auto ltrees = generateTreesRecursive(m, i - 1);
+            auto rtrees = generateTreesRecursive(i + 1, n);
+            for (auto ltree : ltrees) {
+                for (auto rtree : rtrees) {
+                    auto tree   = new TreeNode(i);
+                    tree->left  = ltree;
+                    tree->right = rtree;
+                    trees.emplace_back(tree);
+                }
+            }
+        }
+        return trees;
+    }
+
+   public:
+    std::vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) {
+            return {};
+        }
+        return generateTreesRecursive(1, n);
+    }
+};
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(test, case1) {
+    Solution solution;
+    auto     trees = solution.generateTrees(3);
+    auto     nodes = std::vector<TreeNode*>({
+        CreateTree({1, $, 2, $, 3}),
+        CreateTree({1, $, 3, 2}),
+        CreateTree({2, 1, 3}),
+        CreateTree({3, 1, $, $, 2}),
+        CreateTree({3, 2, $, 1}),
+    });
+    EXPECT_EQ(trees.size(), nodes.size());
+    for (int i = 0; i < trees.size(); i++) {
+        ShowTree(trees[i]);
+        EXPECT_TRUE(EqualsTree(trees[i], nodes[i]));
+    }
+}
+
+TEST(test, case2) {
+    Solution solution;
+    EXPECT_TRUE(solution.generateTrees(0).empty());
+}
