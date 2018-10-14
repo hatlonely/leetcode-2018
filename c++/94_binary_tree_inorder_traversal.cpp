864828cdaf1abba77f574937ada7d2faabdae262
hatlonely <hatlonely@gmail.com>
2018-10-14 23:04:44 +0800

    add c++/94_binary_tree_inorder_traversal.cpp

---
+++ c++/94_binary_tree_inorder_traversal.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
+#include "treenode.hpp"
+
+class Solution {
+    void inorderTraversalRecursive(std::vector<int>& vi, TreeNode* root) {
+        if (root->left != nullptr) {
+            inorderTraversalRecursive(vi, root->left);
+        }
+        vi.emplace_back(root->val);
+        if (root->right != nullptr) {
+            inorderTraversalRecursive(vi, root->right);
+        }
+    }
+
+   public:
+    std::vector<int> inorderTraversal(TreeNode* root) {
+        std::vector<int> vi;
+        if (root != nullptr) {
+            inorderTraversalRecursive(vi, root);
+        }
+        return vi;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(test, case1) {
+    Solution solution;
+    EXPECT_EQ(solution.inorderTraversal(CreateTree({1, $, 2, 3})), std::vector<int>({1, 3, 2}));
+}
---
+++ c++/treenode.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+
+const int $ = -1;
+
+struct TreeNode {
+    int       val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+TreeNode *CreateTree(std::initializer_list<int> li) {
+    TreeNode  prev(0);
+    TreeNode *node = &prev;
+
+    std::queue<TreeNode *> nodes;
+
+    int is_left = false;
+    for (int i : li) {
+        if (is_left) {
+            if (i != $) {
+                node->left = new TreeNode(i);
+                nodes.push(node->left);
+            }
+            is_left = false;
+        } else {
+            if (i != $) {
+                node->right = new TreeNode(i);
+                nodes.push(node->right);
+            }
+            node = nodes.front();
+            nodes.pop();
+            is_left = true;
+        }
+    }
+
+    return prev.right;
+}
+
+void DestroyTree(TreeNode *node) {
+    if (node != nullptr) {
+        DestroyTree(node->left);
+        DestroyTree(node->right);
+        delete node;
+    }
+}
+
+void ShowTree(TreeNode *node) {
+    std::queue<TreeNode *> nodes;
+    nodes.push(node);
+    std::cout << node->val << ", ";
+    while (!nodes.empty()) {
+        node = nodes.front();
+        nodes.pop();
+        if (node->left != nullptr) {
+            nodes.push(node->left);
+            std::cout << node->left->val << ", ";
+        } else {
+            std::cout << "$, ";
+        }
+        if (node->right != nullptr) {
+            nodes.push(node->right);
+            std::cout << node->right->val << ", ";
+        } else {
+            std::cout << "$, ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+bool EqualsTree(TreeNode *node1, TreeNode *node2) {
+    if (node1 == nullptr) {
+        return node2 == nullptr;
+    }
+    if (node2 == nullptr) {
+        return false;
+    }
+    return node1->val == node2->val &&
+           EqualsTree(node1->left, node2->left) &&
+           EqualsTree(node1->right, node2->right);
+}
