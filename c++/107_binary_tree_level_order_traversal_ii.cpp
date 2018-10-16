f0cb23b50f687e73c59f077fee36ab271bc3b66b
hatlonely <hatlonely@gmail.com>
2018-10-16 15:11:38 +0800

    add c++/107_binary_tree_level_order_traversal_ii.cpp

---
+++ c++/107_binary_tree_level_order_traversal_ii.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include "treenode.hpp"
+
+class Solution {
+   public:
+    std::vector<std::vector<int>> levelOrderBottom(TreeNode* root) {
+        std::vector<std::vector<int>> vvi;
+        std::queue<TreeNode*>         tq;
+        if (root != nullptr) {
+            tq.push(root);
+        }
+        while (!tq.empty()) {
+            auto             size = tq.size();
+            std::vector<int> vi;
+            vi.reserve(size);
+            for (int i = 0; i < size; i++) {
+                auto node = tq.front();
+                tq.pop();
+                vi.emplace_back(node->val);
+                if (node->left != nullptr) {
+                    tq.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    tq.push(node->right);
+                }
+            }
+            vvi.emplace_back(vi);
+        }
+        std::reverse(vvi.begin(), vvi.end());
+        return vvi;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(test, case1) {
+    Solution solution;
+    EXPECT_EQ(
+        solution.levelOrderBottom(NewTree({3, 9, 20, $, $, 15, 7})),
+        std::vector<std::vector<int>>({
+            {15, 7},
+            {9, 20},
+            {3},
+        }));
+}
