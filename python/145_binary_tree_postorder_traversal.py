871f3745f6af25ded05b205a0bffbf75f5352603
hatlonely <hatlonely@gmail.com>
2018-10-26 14:32:31 +0800

    add 145_binary_tree_postorder_traversal.py

--- python/145_binary_tree_postorder_traversal.py
+++ python/145_binary_tree_postorder_traversal.py
@@ -2,11 +2,13 @@
 
 from treenode import *
 
+
 class Solution:
     def postorderTraversal(self, root):
         if root is None:
             return []
         return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+
 
 if __name__ == '__main__':
     solution = Solution()
