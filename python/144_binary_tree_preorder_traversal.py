9132afaf4986ded30a551f69b3020ac321ffe803
hatlonely <hatlonely@gmail.com>
2018-10-26 14:32:08 +0800

    add 137_single_number_ii.py

--- python/144_binary_tree_preorder_traversal.py
+++ python/144_binary_tree_preorder_traversal.py
@@ -2,11 +2,13 @@
 
 from treenode import *
 
+
 class Solution:
     def preorderTraversal(self, root):
         if root is None:
             return []
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
 
 if __name__ == '__main__':
     solution = Solution()
