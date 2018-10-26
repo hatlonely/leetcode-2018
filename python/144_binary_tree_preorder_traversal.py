ca5b47aa9702c24cbf1360f25514043c8740823e
hatlonely <hatlonely@gmail.com>
2018-10-26 12:51:23 +0800

    add 144_binary_tree_preorder_traversal.py

---
+++ python/144_binary_tree_preorder_traversal.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from treenode import *
+
+class Solution:
+    def preorderTraversal(self, root):
+        if root is None:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.preorderTraversal(NewTree([1, None, 2, 3])))
