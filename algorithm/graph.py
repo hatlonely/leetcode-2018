b6d5df65840066be123acbcc250c0266dd91dfe4
hatlonely <hatlonely@gmail.com>
2018-10-27 15:33:54 +0800

    add graph.py

--- algorithm/graph.py
+++ algorithm/graph.py
@@ -28,12 +28,15 @@ class Graph(object):
     def __init__(self, nodes, edges):
         self.nodes = []
         self.vnMap = {}
+        self.viMap = {}
         for value in nodes:
             node = GraphNode(value)
             self.vnMap[value] = node
+            self.viMap[value] = len(self.nodes)
             self.nodes.append(node)
         for edge in edges:
-            self.vnMap[edge[0]].addEdge(self.vnMap[edge[1]], edge[2])
+            n1, n2, distance = edge
+            self.vnMap[n1].addEdge(self.vnMap[n2], distance)
 
     def __str__(self):
         return '[' + ', '.join([str(node) for node in self.nodes]) + ']'
@@ -54,6 +57,8 @@ class Graph(object):
                 if mindistance > edge.distance:
                     mindistance = edge.distance
                     node = n
+            if node is None:
+                break
             vset.remove(node)
             for edge in node.edges:
                 if edge.node.value not in emap or emap[edge.node.value].distance > emap[node.value].distance + edge.distance:
@@ -64,15 +69,47 @@ class Graph(object):
 
         for node in self.nodes:
             n2 = node.value
-            if n2 not in emap:
-                yield [], -1
+            if n2 == n1:
+                yield '{}=>{}'.format(n1, n2), 0, [n1]
+            elif n2 not in emap:
+                yield '{}=>{}'.format(n1, n2), -1, []
             else:
                 path = [n2]
                 n = n2
                 while n != n1:
                     n = emap[n].node.value
                     path.append(n)
-                yield path[::-1], emap[n2].distance
+                yield '{}=>{}'.format(n1, n2), emap[n2].distance, path[::-1]
+
+    def floyd(self):
+        n = len(self.nodes)
+        ad = [[None for i in range(n)] for j in range(n)]
+        pd = [[-1 for i in range(n)] for j in range(n)]
+        for i in range(0, n):
+            ad[i][i] = 0
+            pd[i][i] = i
+        for node in self.nodes:
+            for edge in node.edges:
+                i = self.viMap[node.value]
+                j = self.viMap[edge.node.value]
+                ad[i][j] = edge.distance
+                pd[i][j] = i
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if ad[i][k] and ad[k][j] and (ad[i][j] is None or ad[i][j] > ad[i][k] + ad[k][j]):
+                        ad[i][j] = ad[i][k] + ad[k][j]
+                        pd[i][j] = pd[k][j]
+        for i in range(n):
+            for j in range(n):
+                n1 = self.nodes[i].value
+                n2 = self.nodes[j].value
+                k = j
+                path = [n2]
+                while k != i:
+                    k = pd[i][k]
+                    path.append(self.nodes[k].value)
+                yield '{}=>{}'.format(n1, n2), ad[i][j], path[::-1]
 
 
 if __name__ == '__main__':
@@ -89,4 +126,9 @@ if __name__ == '__main__':
         ]
     )
     print(graph)
-    print(list(graph.dijkstra('v0')))
+    print()
+    for i in graph.dijkstra('v0'):
+        print(i)
+    print()
+    for i in graph.floyd():
+        print(i)
