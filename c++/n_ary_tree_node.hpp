88fc5e66bcac5a499a0816ab0b95a8fc67589faa
hatlonely <hatlonely@gmail.com>
2018-11-10 21:48:28 +0800

    add n_ary_tree_node.hpp

---
+++ c++/n_ary_tree_node.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Node {
+   public:
+    int                val;
+    std::vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, std::vector<Node*> _children) {
+        val      = _val;
+        children = _children;
+    }
+};
