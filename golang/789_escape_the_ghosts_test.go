f32e7cc1ce04b17102f5dccc7192b91a6fa4aa66
hatlonely <hatlonely@gmail.com>
2018-11-12 16:21:31 +0800

    add 789_escape_the_ghosts_test.go

---
+++ golang/789_escape_the_ghosts_test.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
+
+func escapeGhosts(ghosts [][]int, target []int) bool {
+	d0 := abs(target[0]) + abs(target[1])
+	for _, ghost := range ghosts {
+		di := abs(ghost[0]-target[0]) + abs(ghost[1]-target[1])
+		if d0 >= di {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Test(t *testing.T) {
+	Convey("test", t, func() {
+
+	})
+}
